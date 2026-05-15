be59292797dad8b225d97e689047c3395220e868
tech-shivendra <techshivendraa@gmail.com>
2026-05-15 11:48:07 +0530

    Refactor VideoEditor component placement in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,11 @@ export default function Home() {
       >
         ⭐ Star on GitHub
       </a>
+      
       <VideoEditor />
-    </>
-  );
-}
     <main>
       <VideoEditor />
     </main>
+    </>
   );
 }
